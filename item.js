43b1f97f130ae56ea523113daa10b9ea619571f3
syko99 <80788702+syko99@users.noreply.github.com>
2022-04-29 17:43:45 -0500

    Add files via upload

---
+++ item.js
@@ -0,0 +1,18 @@
+var productImg = document.getElementById("productImg");
+var colorway = document.getElementsByClassName("colorway");
+
+colorway[0].onclick = function(){
+    productImg.src = colorway[0].src;
+}
+colorway[1].onclick = function(){
+    productImg.src = colorway[1].src;
+}
+colorway[2].onclick = function(){
+    productImg.src = colorway[2].src;
+}
+colorway[3].onclick = function(){
+    productImg.src = colorway[3].src;
+}
+colorway[4].onclick = function(){
+    productImg.src = colorway[4].src;
+}
